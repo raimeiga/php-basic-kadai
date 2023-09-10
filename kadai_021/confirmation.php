eef5fb5f7d11b0fbfc15a8ff4e68ccf31ebd8a49
raimeiga <yusuke612@hotmail.com>
2023-09-10 09:52:40 +0900

    最初の提出

---
+++ kadai_021/confirmation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+  
+ <body>
+     <h2>入力内容をご確認ください。</h2>
+     <p>お名前：<?php echo $_POST['user_name']; ?></p>
+     <p>性別：<?php echo $_POST['user_gender']; ?></p>
+     <p>メールアドレス：<?php echo $_POST['user_email']; ?></p>
+    
+</body>
+ 
+ </html>
